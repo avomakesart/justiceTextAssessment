957a043fbad17aace0ddfe1f898cc0b441b25ad9
Alvaro Castillo <alvaro.castillo777@gmail.com>
2021-03-31 01:16:00 -0600

    Putting back the 8080 port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const http = require('http').createServer(app);
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 8080;
 
 const data = require('./data.json');
 
