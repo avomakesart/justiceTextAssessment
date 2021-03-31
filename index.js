14367803adfae90b5814a1e040f01da028984326
Alvaro Castillo <alvaro.castillo777@gmail.com>
2021-03-30 23:11:44 -0600

    Improving the backend performance and adding tailwindcss configs on the frontend

--- index.js
+++ index.js
@@ -2,32 +2,37 @@ const express = require('express');
 
 const app = express();
 const http = require('http').createServer(app);
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 7000;
 
-const data = require('./data.json')
+const data = require('./data.json');
 
-const DATA_SIZE_HALF = "half"
-const DATA_SIZE_FULL = "full"
+const DATA_SIZE_HALF = 'half';
+const DATA_SIZE_FULL = 'full';
 
-app.get("/api/dataIdList", (req, res) => {
+app.get('/api/dataIdList', (req, res) => {
   if (!req.query.datasize) {
-    res.status(400).send('Bad Request - missing query')
+    res.status(400).send('Bad Request - missing query');
     return;
   }
 
   if (req.query.datasize === DATA_SIZE_HALF) {
-    res.send(data.rowIdHalfList)
+    console.log('halfList: ', data.rowIdHalfList);
+    res.send(data.rowIdHalfList);
+  } else if (req.query.datasize === DATA_SIZE_FULL) {
+    console.log('fullList: ', data.rowIdFullList);
+    res.send(data.rowIdFullList);
   } else {
-    res.send(data.rowIdFullList)
+    console.log('else', data.rows['row' + req.query.datasize]);
+    res.send(data.rows['row' + req.query.datasize]);
   }
-})
+});
 
-app.get("/api/dataItem/:id", (req, res) => {
+app.get('/api/dataItem/:id', (req, res) => {
   if (!req.params.id) {
-    res.status(400).send('Bad Request - missing id')
+    res.status(400).send('Bad Request - missing id');
     return;
   }
-  res.send(data.rows["row" + req.params.id])
-})
+  res.send(data.rows['row' + req.params.id]);
+});
 
 http.listen(port, () => console.log(`Listening on port ${port}`));
